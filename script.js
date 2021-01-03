7bd957f9b5d5c686866838c2e7e9c1c7f617c850
huyanlong <2536620695@qq.com>
2021-01-03 20:25:55 +0800

    增加微服务页面，调整css

--- script.js
+++ script.js
@@ -10,20 +10,35 @@ $(function(){
       tag2: '12-3截止',
       tag3: 'Pinddig',
       desc: '规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述',
+    },
+    {
+      id: 2,
+      group: 'NFVO',
+      name: '重构规划名称',
+      authorName: '杨连刚',
+      authorUrl: './avatar.jpeg',
+      tag1: '30K',
+
+
+      tag2: '12-3截止',
+      tag3: 'Pinddig',
+      desc: '我爱我家',
     }
   ]
   let html = getCardHtml(data)
-  $('#access .container-item-title span').text(data.length)
-  $('#access .container-item-body').html(html)
+  $('#access .content-item-title span').text(data.length)
+  $('#access .content-item-body').html(html)
 })
 
 function getCardHtml (data) {
   let html = ''
   data.forEach(item => {
     let card = `<div class="card">
-      <div class="card-title">
+      <div class="card-header">
+        <div class="card-header-left">
         <span class="tag tag-blue">${item.group}</span>
-        <span class="card-title-text">${item.name}</span>
+        <span class="card-header-left-text">${item.name}</span>
+      </div>  
       </div>
       <div class="card-author">
         <img class="avatar" src="${item.authorUrl}" alt="">
@@ -39,4 +54,45 @@ function getCardHtml (data) {
     html += card
   })
   return html
+}
+
+function getHtmlByType() {
+  // 定义三种不同类型的card
+  // 1. 存在vnfo的card
+  let card1 = `<div class="card">
+              <div class="card-header-left">
+                <span class="tag tag-blue">NFVO</span>
+                <span class="card-header-left-text">重构规划名称</span>
+              </div>
+              <div class="card-author">
+                <img class="avatar" src="./avatar.jpeg" alt="">
+                <span class="name">杨连刚</span>
+                <span class="tag tag-info">30K</span>
+                <span class="tag tag-warning">12-3截止</span>
+<!--                <span class="tag tag-error">Pinddig</span>-->
+              </div>
+              <div class="card-content">
+                规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述
+              </div>
+            </div>`;
+
+  // CMDB
+  let card2  = `<div class="card">
+              <div class="card-header-left">
+<!--                <span class="tag tag-blue">NFVO</span>-->
+                <span class="card-header-left-text">CMDB交互工具CBB</span>
+              </div>
+              <div class="card-author">
+                <img class="avatar" src="./avatar.jpeg" alt="">
+                <span class="name">杨连刚</span>
+                <span class="tag tag-info">30K</span>
+                <span class="tag tag-warning">12-3截止</span>
+                <span class="tag tag-error">Pinddig</span>
+              </div>
+              <div class="card-content">
+                规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述规划描述
+              </div>
+            </div>`;
+
+
 }
